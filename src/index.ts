b901fcd2e00f72f50598d1a6bafa8bd2f83fb91e
Intrstng <Intrstng@gmail.com>
2022-12-27 23:38:05 +0300

    init: init TS project

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { test } from './test' // чтобы не писать расширения файлов, надо в webpack.config.js добавить
+// resolve: {
+//   extensions: ['.tsx', '.ts', '.js'],
+// },
+console.log('Start!');
